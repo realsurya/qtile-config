adf86406998170694b011db831191779b063f003
Surya Mani <57917954+realsurya@users.noreply.github.com>
2022-05-06 01:08:28 -0400

    Better config

--- config.py
+++ config.py
@@ -11,7 +11,7 @@
 #          /:/  /       \:\__\          #
 #          \/__/         \/__/          #
 #                                       #
-#           ~My Qtile Config~           #
+#            ~My Qtile Config~           #
 #             Feb. 04, 2022             #
 #                                       #
 #           Surya Manikhandan           #
@@ -29,6 +29,11 @@ from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
+#---------------------------------------#
+#             COLOR SCHEHE              #
+#---------------------------------------#
+black = '#000000'
+transparent = '#00000000'
 
 #---------------------------------------#
 #            BASE KEYBINDS              #
@@ -38,25 +43,29 @@ mod = "mod4"
 terminal = guess_terminal()
 
 keys = [
-    # Ref: https://docs.qtile.orgm/en/latest/manual/config/lazy.html
-    # Toggle focus between windows.  
-    Key([mod], "Left", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "Right", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "Down", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "Up", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    # My Ref: https://docs.qtile.orgm/en/latest/manual/config/lazy.html
+
+    # Toggle focus between windows.
+    Key([mod], "Left", lazy.layout.left(), desc="Switch focus left"),
+    Key([mod], "Right", lazy.layout.right(), desc="Switch focus right"),
+    Key([mod], "Down", lazy.layout.down(), desc="Switch focus down"),
+    Key([mod], "Up", lazy.layout.up(), desc="Switch focus up"),
+    Key([mod], "space", lazy.layout.next(), desc="Switch window focus to other window"),
+
     # Move windows within columns or shuffle in current stack.
     Key([mod, "shift"], "Left", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "Right", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "Down", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "Up", lazy.layout.shuffle_up(), desc="Move window up"),
+
     # Manipulate window sizes.
-    Key([mod, "control"], "Left", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "Right", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "Down", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "Up", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod, "control"], "Left", lazy.layout.grow_left(), desc="Extend stack to the left"),
+    Key([mod, "control"], "Right", lazy.layout.grow_right(), desc="Extend stack to the right"),
+    Key([mod, "control"], "Down", lazy.layout.grow_down(), desc="Extend window down"),
+    Key([mod, "control"], "Up", lazy.layout.grow_up(), desc="Extend window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # toggle priority of seected window in stack.
+
+    # toggle priority of sleected window in stack.
     Key(
         [mod, "shift"],
         "Return",
@@ -64,41 +73,37 @@ keys = [
         desc="Toggle between split and unsplit sides of stack",
     ),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "shift"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "shift"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod], "d", lazy.spawn('rofi -theme gruvbox-dark-hard -font "hack 24" -show run -icon-theme "Papirus" -show-icons'), desc="Spawn rofi"),
 ]
-
 
 #---------------------------------------#
 #              WORKSPACES               #
 #---------------------------------------#
-groups = [Group(i) for i in "123456789"]
+groups = [Group(i) for i in "12345"]
 
 for i in groups:
     keys.extend(
         [
-            # mod1 + letter of group = switch to group
+            # Pure workspace shifting
             Key(
                 [mod],
                 i.name,
                 lazy.group[i.name].toscreen(),
                 desc="Switch to group {}".format(i.name),
             ),
-            # mod1 + shift + letter of group = switch to & move focused window to group
+            # Move windows between workspaces
             Key(
                 [mod, "shift"],
                 i.name,
                 lazy.window.togroup(i.name, switch_group=True),
                 desc="Switch to & move focused window to group {}".format(i.name),
             ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
         ]
     )
 
@@ -127,7 +132,7 @@ layouts = [
 #---------------------------------------#
 widget_defaults = dict(
     font="sans",
-    fontsize=26,
+    fontsize=28,
     padding=20,
 )
 extension_defaults = widget_defaults.copy()
@@ -136,25 +141,17 @@ screens = [
     Screen(
         top=bar.Bar(
             [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ffffff", "#ff0000"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.TextBox("| Surya M |", name="default"),     
-                widget.Systray(),
-                widget.Clock(format="| %I:%M %p - %a %m/%d |"),
-                widget.Battery(format='| Bat: {percent:2.0%} ({hour:d}:{min:02d} {char}) |', charge_char="▲", discharge_char="▼", update_interval=3),
-                widget.QuickExit(default_text="| Shutdown |", countdown_format ='| {} Seconds |')
+                widget.CurrentLayout(background=black),
+                widget.GroupBox(highlight_method='line', background=black),
+                widget.Spacer(length=bar.STRETCH),
+                widget.Clock(format=" ~   %a %m/%d  %I:%M %p   ~ ", background=black),
+                widget.Spacer(length=bar.STRETCH),
+                widget.Battery(format=' Bat1: {percent:2.0%}', update_interval=5, battery=1, background=black),
+                widget.Battery(format='Bat2: {percent:2.0%} ', update_interval=5, battery=2, background=black),
             ],
             45,
-            background='#000000',
-            margin=[5,5,5,5]
+            background=transparent,
+            margin=[0,0,0,0]
         ),
     ),
 ]
